f7bb715192d19c1680810fbe7d449b161cefff70
shreyag12 <gupta.shreya1203@gmail.com>
2018-01-21 12:25:37 +0530

    final commit

--- mysite/bookmark/views.py
+++ mysite/bookmark/views.py
@@ -26,12 +26,6 @@ def register(request):
         if form.is_valid():
             user = form.save()
             request.session['user'] = user.pk
-            # user=User.objects.create_user(
-            # email=form.cleaned_data['email'],
-            # password = form.cleaned_data['Password'],
-            # password2 = form.cleaned_data['Confirm password']
-            # )
-            # return HttpResponseRedirect('/register/success/')
             return render(request,'category.html')
 # If this is a GET (or any other method) create the default form.
     else:
@@ -47,7 +41,8 @@ def login_view(request):
         args = {'user': request.user}
         login(request,user)
         blist = get_bookmark(user)
-        return render(request,'myprofile.html',{'blist': blist})
+        firstname = get_firstname(request)
+        return render(request,'myprofile.html',{'blist': blist,'firstname':firstname})
     return render(request,'login.html',{'form': form})
 
 def register_success(request):
@@ -76,7 +71,8 @@ def save_bookmark(request):
     obj = UserBookmark(user_id=user,bookmark=bmark)
     obj.save()
     blist = get_bookmark(user)
-    return render(request,'myprofile.html',{'blist': blist})
+    firstname = get_firstname(request)
+    return render(request,'myprofile.html',{'blist': blist,'firstname':firstname})
 
 def get_bookmark(user):
     blist = UserBookmark.objects.filter(user = user)
@@ -86,7 +82,7 @@ def home(request):
     category=Category.objects.values_list('category',flat = True).filter(user=request.user.pk)
     category = list(category)
     category2=ast.literal_eval(category[0])
-    # print category2
+    print category2
     # category = category.split(',')
     # category2 = []
     # for x in category:
@@ -98,12 +94,12 @@ def home(request):
 
     # bookmarks= UserBookmark.objects.values('bookmark').filter(tag__in = category).exclude(user=request.user.pk)
     # bookmarks = UserBookmark.objects.values('bookmark').filter(tag__in = ['java','android','python'])
-    bookmarks = UserBookmark.objects.filter(tag__name__in = category2).exclude(user = request.user.pk)
-    print bookmarks
+    bookmarks = UserBookmark.objects.filter(tag__name__in = category2).exclude(user = request.user.pk).distinct()
     interests = Category.objects.values_list('category').filter(user = request.user.pk)
     interests = list(interests)
     interests= ast.literal_eval(interests[0][0])
-    blist = UserBookmark.objects.values('bookmark').filter(tag__name__in = category2).exclude(user = request.user.pk)
+    blist = UserBookmark.objects.values('bookmark').filter(tag__name__in = category2).exclude(user = request.user.pk).distinct()
+    print blist
     mydict = {}
     for i in blist:
         tag_ids = UserBookmark.objects.values('tag').filter(bookmark = i['bookmark'])
@@ -122,12 +118,14 @@ def home(request):
     # print mydict
     # for key in mydict:
     #     print mydict[key]
-    print mydict["https://simpleisbetterthancomplex.com/tutorial/2016/07/26/how-to-reset-migrations.html"]
-    return render(request,'home.html',{'mydict': mydict,'bookmarks' :bookmarks, 'interests':interests})
+    # firstname = User.objects.values_list('first_name').filter(username= request.user.username)
+    # firstname = list(firstname)
+    # firstname = str(firstname[0][0])
+    firstname = get_firstname(request)
+    return render(request,'home.html',{'mydict': mydict,'bookmarks' :bookmarks, 'interests':interests,'firstname':firstname})
     # return render(request,'home.html',{'mydict':mydict})
 
 def save_tag(request):
-    # tagslist = request.POST.getlist("tag")
     tagslist = request.POST.get("tag")
     tagslist = tagslist.split(',')
     bookmark = request.POST.get("bookmark")
@@ -145,26 +143,64 @@ def delete_bookmark(request,bookmark):
     # bookmark = request.POST.get("value")
     UserBookmark.objects.filter(user = user,bookmark = bookmark).delete()
     blist = get_bookmark(user)
-    return render(request,'myprofile.html',{'blist' : blist})
+    firstname = get_firstname(request)
+    return render(request,'myprofile.html',{'blist' : blist,'firstname':firstname})
 def add_bookmark(request,bookmark): 
     user = request.user.pk
     obj=UserBookmark(user_id=user,bookmark = bookmark)
     obj.save()
     bookmarks = show(user)
-    return render(request,'home.html',{'bookmarks':bookmarks})
+    mydict = get_dict(request)
+    interests = get_interest(request)
+    firstname = get_firstname(request)
+    return render(request,'home.html',{'mydict': mydict,'bookmarks' :bookmarks, 'interests':interests,'firstname':firstname})
 
 def show(user):
     category=Category.objects.values_list('category',flat = True).filter(user=user)
     category = list(category)
     category2=ast.literal_eval(category[0])
-    bookmarks = UserBookmark.objects.filter(tag__name__in = category2).exclude(user = user)
+    bookmarks = UserBookmark.objects.filter(tag__name__in = category2).exclude(user = user).distinct()
     return bookmarks
 def profile(request):
     user = request.user.pk
     blist = get_bookmark(user)
-    return render(request,'myprofile.html',{'blist': blist})
+    firstname = get_firstname(request)
+    print firstname
+    return render(request,'myprofile.html',{'blist': blist,'firstname':firstname})
 def about(request):
     return render(request,'about.html')
+def get_firstname(request):
+    firstname = User.objects.values_list('first_name').filter(username= request.user.username)
+    firstname = list(firstname)
+    firstname = str(firstname[0][0])
+    return firstname
+def get_dict(request):
+    category=Category.objects.values_list('category',flat = True).filter(user=request.user.pk)
+    category = list(category)
+    category2=ast.literal_eval(category[0])
+    interests = Category.objects.values_list('category').filter(user = request.user.pk)
+    interests = list(interests)
+    interests= ast.literal_eval(interests[0][0])
+    blist = UserBookmark.objects.values('bookmark').filter(tag__name__in = category2).exclude(user = request.user.pk)
+    mydict = {}
+    for i in blist:
+        tag_ids = UserBookmark.objects.values('tag').filter(bookmark = i['bookmark'])
+        bookmark = i['bookmark']
+        tags = Tag.objects.values_list('name',flat=True).filter(id__in = tag_ids)
+        tags = list(tags)
+        new = []
+        for t in tags:
+            n=t.encode('ascii')
+            new.append(n)
+        mydict[bookmark]=new
+    return mydict
+def get_interest(request):
+    interests = Category.objects.values_list('category').filter(user = request.user.pk)
+    interests = list(interests)
+    interests= ast.literal_eval(interests[0][0])
+    return interests
+
+
 
 
 
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-#import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +25,7 @@ SECRET_KEY = '_8y7cx1(-l!5+$j$v6+p!t7ukm_u0&0loq(m#0yzoe8gpjr5=^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['learnitgirl-bookmark.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -121,5 +120,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
